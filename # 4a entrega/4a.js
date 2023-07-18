65070de9a24d272d6e253e0ef0c35186028c3380
Daphoenyx <98786403+Daphoenyx@users.noreply.github.com>
2023-07-18 05:09:36 +0200

    Add files via upload

---
+++ # 4a entrega/4a.js
@@ -0,0 +1,32 @@
+
+
+const mostrarJuegos = (juegos) => {
+    const contenedorJuegos = document.querySelector(".data");
+    contenedorJuegos.innerHTML = "";
+    juegos.forEach((juego) => {
+        const div = document.createElement ("div");
+        div.innerHTML = `<h3>${juego.nombre}</h3>
+                        <img src = "${juego.img}" </img>
+                        <h5>${juego.categoria}</h5>
+                        <h5>${juego.anio}</h5>
+                        <h4>${juego.precio} euros</h4>
+                        <button id = '${juego.id}' class ="comprar"> COMPRAR </button>`;
+    contenedorJuegos.appendChild(div);
+    const boton = document.getElementById(`${juego.id}`);
+    boton.addEventListener("click", () => {
+			agregar(juego.id);
+                    })
+    })
+}
+
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const agregar = (id) => {
+    if (!carrito.some((juego) => juego.id === id)) {
+        const juego = juego.find((juego) => juego.id === id)
+        carrito.push(juego)
+    }
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    mostrarCarrito()
+}
